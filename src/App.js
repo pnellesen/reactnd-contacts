a426a76b47084b5f4c7b17c45458c1ef1b7bc25f
PNellesen <pnellesen@hotmail.com>
2018-06-12 21:49:06 -0500

    Just playing around

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class ContactList extends Component {
+  
+  render() {
+    
+    let myArray = [
+      'contact 1',
+      'contact 2',
+      'contact 3'
+    ];
+    
+    return (
+        <ul>{myArray.map(thisContact => <li key={thisContact}>{thisContact}</li>)}</ul>
+    )
+  }
+}
+
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +31,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>Starter contact list</h2>
+        <ContactList/>
       </div>
     );
   }
